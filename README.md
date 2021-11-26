236e0e97413e59afb9a35a37fe77a954d4e7d95d
Mr-Destructive <gormeet711@gmail.com>
2021-11-26 15:23:36 +0530

    Added button and toggle component

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ### A collection of some custom svelte components
 
+### List of components
 
+- [Ripple Effect Button](./src/components/Button.svelte)
+- [Dark/Light Mode Toggle](./src/components/Toggle-Mode.svelte)
--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,30 @@
 <script>
-	export let name;
+    import Button from './components/Button.svelte'
+    import ToggleMode from './components/Toggle-Mode.svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+    <h1>Svelte Components</h1>
+    <ul>
+        <li><h3>Ripple Effect Button</h3><Button/></li>
+        <li><h3>Dark-Light Mode Toggle</h3><ToggleMode/></li>
+    </ul>
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
+    :global(body.dark){
+        background-color:black;
+        color:white;
+    }
+
 	h1 {
+        text-align: center;
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
